c03b83e85fd8d2cccd37e0bfeba4d2025bd7498b
CPE 586 Machine Learning <cricket.uah99@gmail.com>
2025-09-27 06:03:34 -1200

    Create hw02 package

---
+++ hw02/__init__.py
@@ -0,0 +1,8 @@
+"""
+HW02 Python Package for CPE 486/586
+Cryptographically Secure Distributions
+"""
+
+from .distributions import uniform, exponentialdist, poissondist
+
+__all__ = ['distributions', 'uniform', 'exponentialdist', 'poissondist']
